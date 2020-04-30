532ad5b031b3cd06f322041309a4dfac17b49f46
NicoSchlueter <30301307+NicoSchlueter@users.noreply.github.com>
2020-04-30 15:46:11 +0200

    Initial commit

---
+++ OldSketch.py
@@ -0,0 +1,76 @@
+#Author-Nico Schlueter
+#Description-Adds the Sketch>Design toolbar to the Solid tab, similar to how it was before the tabbed toolbar.
+
+import adsk.core, adsk.fusion, adsk.cam, traceback
+
+# API does not allow getting Dropdown names
+names = {
+    'RectangleDropDown' : "Rectangle",
+    'CircleDropDown' : "Circle",
+    'ArcDropDown' : "Arc",
+    'PolygonDropDown' : "Polygon",
+    'SlotDropDown' : "Slot",
+    'SplineDropDown' : "Spline",
+    'ProjectIncludeDropDown' : "Project / Include"
+}
+
+
+def run(context):
+    ui = None
+    try:
+        ui = adsk.core.Application.get().userInterface
+
+        sketchCreatePanel = ui.allToolbarPanels.itemById("SketchCreatePanel")
+        solidTab = ui.allToolbarTabs.itemById("SolidTab")
+
+        oldSketchCreatePanel = solidTab.toolbarPanels.add(
+            "OldSketchCreatePanel",
+            sketchCreatePanel.name,
+            "SolidCreatePanel",
+            True
+        )
+
+        for i in sketchCreatePanel.controls:
+            addControl(oldSketchCreatePanel.controls, i)
+
+    except:
+        print(traceback.format_exc())
+
+
+def stop(context):
+    ui = None
+    try:
+        ui  = adsk.core.Application.get().userInterface
+
+        oldSketchCreatePanel = ui.allToolbarPanels.itemById("OldSketchCreatePanel")
+        if oldSketchCreatePanel:
+            oldSketchCreatePanel.deleteMe()
+
+    except:
+        print(traceback.format_exc())
+
+
+def addControl(controls, control):
+    if(control.objectType == "adsk::core::CommandControl"):
+        con = controls.addCommand(control.commandDefinition)
+        con.isPromoted = control.isPromoted
+        con.isPromotedByDefault = control.isPromotedByDefault
+
+    elif(control.objectType == "adsk::core::DropDownControl"):
+        con = controls.addDropDown(
+            getNameFromId(control.id),
+            "" #TODO: Extract icons
+        )
+
+        for i in control.controls:
+            addControl(con.controls, i)
+
+    elif(control.objectType == "adsk::core::SeparatorControl"):
+        controls.addSeparator()
+
+
+def getNameFromId(controlID):
+    global name
+    if(controlID in names):
+        return names[controlID]
+    return controlID
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # OldSketch
-Fusion360 Add-In that displays the SketchCreate panel in the Solid Toolbar
+A simple Add-In for Autodesk Fusion360 that shows the SketchCreate ToolbarPanel in the Solid Toolbar, similar to how it was before the Tabbed Toolbar was introduced.
+
+<img width="1440" alt="Screenshot 2020-04-30 at 15 28 44" src="https://user-images.githubusercontent.com/30301307/80715950-612b3c00-8af7-11ea-9699-d1021af08605.png">
+
+# Features
+* Copys all controls from the SketchCreate Panel
+* Promotes the same controls that are promoted in the SketchCreate Panel*
+
+# Limitations
+* Promoted controls may be in a different order
+* Dropdowns have no icon
+
+# Installation
+* Download the Project as ZIP and extract it somewhere you can find again, but won't bother you. (or use git to clone it there)
+* Open Fusion360 and press ADD-INS > Scripts and Add-ins
+* Select the tab Add-Ins and click the green plus symbol next to "My Add-Ins"
+* Navigate to the extracted Project forlder and hit open
+* The Add-in should now appear in the "My Add-Ins" list. Select it in the list. If desired check the "Run ond Startup" checkbox and hit run.
+
+# Changelog
+
+## 1.0 Release
+- Initial release
